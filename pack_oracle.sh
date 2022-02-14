535381f2a068bd455f652257c5685d293cbb895f
Open Contracts <90533597+open-contracts@users.noreply.github.com>
2022-02-14 15:53:27 -0800

    Update pack_oracle.sh

--- pack_oracle.sh
+++ pack_oracle.sh
@@ -1,34 +1,46 @@
+  GNU nano 4.8                                                                                             pack_oracle.sh
 #!/bin/bash
-
 set -e
-echo "select the oracle folder: (enter a number)"
-select FOLDER in $(echo */)
+
+echo "{" > oracleIDs.json
+
+for FOLDER in $(echo */)
 do
-FOLDER=${FOLDER%?}
-
-if [ "$FOLDER" = "" ]; then
-  echo "Enter a number."
-else
-  cd $FOLDER
-
-  echo "should I also download the pip requirements? (enter a number)"
-  select DL in yes no
-  do
-  if [ "$DL" = "yes" ]; then
+cd $FOLDER
+  if [ -f "requirementss.txt" ]
+  then
     mkdir -p pip_wheels
     pip3 install wheel
     pip3 wheel  --prefer-binary --wheel-dir pip_wheels -r requirements.txt
     tar -czvf - pip_wheels | split -b 32M - pip_wheels.tar.gz
     rm -rf ./pip_wheels
-    echo 0x$(cat $(find . -type f | sort) | sha256sum | awk '{print $1}') | tee ../oracleID_$FOLDER.txt
-    break
-  elif [ "$DL" = "" ]; then
-    echo "Enter a number."
-  else
-    echo 0x$(cat $(find . -type f | sort) | sha256sum | awk '{print $1}') | tee ../oracleID_$FOLDER.txt
-    break
   fi
-  done
-  break
-fi
+  if [ -f "oracle.py" ]
+  then
+    NAME=$(echo $FOLDER | grep -Eo [^/]*)
+    ID=$(cat $(find . -type f | sort) | sha256sum | awk '{print $1}')
+    echo  '    "'$NAME'": "0x'$ID'",' >> ../oracleIDs.json
+  fi
+cd ..
 done
+sed -i '$ s/.$//' oracleIDs.json
+echo "}" >> oracleIDs.json
+echo ""
+echo "Success! Created oracleIDs.json:"
+cat oracleIDs.json
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
