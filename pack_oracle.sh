6c465e1dd638b3c6511fd380ce8639c015d63bc0
Open Contracts <90533597+open-contracts@users.noreply.github.com>
2022-02-14 16:01:52 -0800

    Update pack_oracle.sh

--- pack_oracle.sh
+++ pack_oracle.sh
@@ -6,16 +6,16 @@ echo "{" > oracleIDs.json
 for FOLDER in $(echo */)
 do
 cd $FOLDER
-  if [ -f "requirementss.txt" ]
-  then
-    mkdir -p pip_wheels
-    pip3 install wheel
-    pip3 wheel  --prefer-binary --wheel-dir pip_wheels -r requirements.txt
-    tar -czvf - pip_wheels | split -b 32M - pip_wheels.tar.gz
-    rm -rf ./pip_wheels
-  fi
   if [ -f "oracle.py" ]
   then
+    if [ -f "requirements.txt" ]
+    then
+      mkdir -p pip_wheels
+      pip3 install wheel
+      pip3 wheel  --prefer-binary --wheel-dir pip_wheels -r requirements.txt
+      tar -czvf - pip_wheels | split -b 32M - pip_wheels.tar.gz
+      rm -rf ./pip_wheels
+    fi
     NAME=$(echo $FOLDER | grep -Eo [^/]*)
     ID=$(cat $(find . -type f | sort) | sha256sum | awk '{print $1}')
     echo  '    "'$NAME'": "0x'$ID'",' >> ../oracleIDs.json
